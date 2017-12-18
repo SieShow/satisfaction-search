fabf82b7fd24e44e097c0d4693d0712e7fb9b447
Lucas Gomes <lucasgsm88@gmail.com>
2017-12-18 21:14:46 -0200

    adicionando novamente as pastas

--- Management/Scripts/ScriptDataFix/ScriptDataFix/Program.cs
+++ Management/Scripts/ScriptDataFix/ScriptDataFix/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MySql.Data.MySqlClient;
+
 namespace ScriptDataFix
 {
     class Program
@@ -16,38 +17,41 @@ namespace ScriptDataFix
             Conection.Port1 = "";
             List<String> ids = new List<string>();
             Console.WriteLine("Conection state: " + Conection.Open());
+            
+            //Console.WriteLine("Collecting informations from CUSTOMER");
 
-            Console.WriteLine("Collecting informations from CUSTOMER");
+            //MySqlCommand command = new MySqlCommand("select distinct idcustomer from form", Conection.Connection);
+            //using (MySqlDataReader data = command.ExecuteReader())
+            //{
+            //    while (data.Read())
+            //    {
+            //        ids.Add(data["idcustomer"].ToString());
+            //    }
+            //}
+            //Console.WriteLine("Coletado " + ids.Count + " suarios");
+            //foreach (string id in ids)
+            //{
+            //    Console.WriteLine("Coletando informações para o id " + id);
+            //    Conection.Close();
+            //    Conection.Open();
+            //    command.CommandText = "select ROUND(avg(evaluation_value), 1) from form where idcustomer = " + id;
+            //    command.Connection = Conection.Connection;
 
-            MySqlCommand command = new MySqlCommand("select distinct idcustomer from form", Conection.Connection);
-            using (MySqlDataReader data = command.ExecuteReader())
-            {
-                while (data.Read())
-                {
-                    ids.Add(data["idcustomer"].ToString());
-                }
-            }
-            Console.WriteLine("Coletado " + ids.Count + " suarios");
-            foreach (string id in ids)
-            {
-                Console.WriteLine("Coletando informações para o id " + id);
-                Conection.Close();
-                Conection.Open();
-                command.CommandText = "select ROUND(avg(evaluation_value), 1) from form where idcustomer = " + id;
-                command.Connection = Conection.Connection;
+            //    string val = command.ExecuteScalar().ToString();
 
-                string val = command.ExecuteScalar().ToString();
+            //    if (val == null || val == "") val = "0";
+            //    val = val.Replace(',', '.');
 
-                if (val == null || val == "") val = "0";
-                val = val.Replace(',', '.');
+            //    Console.WriteLine("id " + id + " possui uma média de " + val + ". Atualizando registro...");
+            //    Conection.Close();
+            //    Conection.Open();
+            //    command.CommandText = "update customer set avaliation_avarage = " + val + " where v11_id = " + id;
+            //    command.Connection = Conection.Connection;
+            //    command.ExecuteNonQuery();
+            //}
 
-                Console.WriteLine("id " + id + " possui uma média de " + val + ". Atualizando registro...");
-                Conection.Close();
-                Conection.Open();
-                command.CommandText = "update customer set avaliation_avarage = " + val + " where v11_id = " + id;
-                command.Connection = Conection.Connection;
-                command.ExecuteNonQuery();
-            }
+
+
 
             Console.WriteLine("Atualização terminada");
             Console.ReadKey();
