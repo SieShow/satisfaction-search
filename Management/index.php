94ca89b0d606f0fe3043fa256ea399812908993e
Lucas Gomes <lucasgsm88@gmail.com>
2017-12-18 21:17:10 -0200

    adicionando pastas

---
+++ Management/index.php
@@ -0,0 +1,36 @@
+<?php
+include('php/login.php');
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8"; />
+        <link href="css/indexstyle.css" rel="stylesheet" type="text/css"/>
+            <link href="../Img/logo.ico" rel="icon" type"image/x-icon" />
+        <title>Login</title>
+    </head>
+    <body>
+        <form action="" method="POST">
+            <div id="formcontents">
+            <label>Usuário</label>
+            <input type="text" name="login" required/>
+            <label>Senha</label>
+        <input style="margin-left: 10px;" type="password" name="passw" onkeypress="capLock(event)" required/>
+            <div id="subdiv">
+        <input style="font-weight: bold;" name="btnlogin" type="submit" value="Entrar"/>
+                  <span><?php echo $error; ?></span>
+                </div>    
+            </div>    
+<!--            <div id="divMayus" style="visibility:hidden; Z-index:10;">Caps Lock is on.</div>  -->  
+    </form>
+    <script>
+    function capLock(e){
+        var kc = e.keyCode ? e.keyCode : e.which;
+        var sk = e.shiftKey ? e.shiftKey : kc === 16;
+        var visibility = ((kc >= 65 && kc <= 90) && !sk) || 
+        ((kc >= 97 && kc <= 122) && sk) ? 'visible' : 'hidden';
+        document.getElementById('divMayus').style.visibility = visibility
+}
+    </script>
+    </body>
+</html>
---
+++ Management/pages/mainfunc.php
@@ -0,0 +1,86 @@
+<?php
+include('../php/DataBaseQuerys.php');
+include('../php/updateprofile.php');
+include('../php/PageMainValidation.php');
+LoginValidation();
+?>
+<!DOCTYPE HTML>
+<HTML>
+<head>
+    <meta charset = "utf-8"/>
+    <link href="../CSS/mainPagesStyle.css" rel="stylesheet" type="text/css" />
+    <link href="../Img/logo.ico" rel="icon" type"image/x-icon" />
+    <link href="https://fonts.googleapis.com/css?family=Lato: 100,300,400,700|Luckiest+Guy|Oxygen:300,400" rel="stylesheet">
+    <title>Gerenciamento</title>
+</head>
+<body>
+    <div id="headpage">
+        <div id="usernameoption">
+        <img src="../Img/User-unknown.png" id="imgclick" >
+        <div id="full-options-block">
+            <div id="arrow">
+                </div>
+                <div id="userinfo">
+                    <img src="../Img/User-unknown.png">
+                    <label><?php echo $user ?></label>
+                </div>     
+        <a href="">Editar perfil</a>
+        <a href="../PHP/Logout.php">Sair</a>
+        </div>
+        </div>
+    <div id="menu">
+        <div id="menuhorizontal">
+        <ul>
+            <li><a href="main.php">Inicial</a></li>
+            <li><a href="mainclientes.php">Clientes</a></li>
+            <li><a href="">Funcionários</a></li>
+            <li><a href="mainfomsanswereds.php">Formulários</a></li>
+            </ul>
+        </div>
+    </div>
+    </div>
+    <div class="allblock">
+        <div id="searchdiv">
+    <input type="text" placeholder="Ache os funcionários !" name="pesquisa"/>
+        </div>
+    <table>
+        <thead id="thead">
+        <td id="nametd">Nome</td>
+        <td>Nota média</td>
+        <td>Média de efetividade do atendimento</td>
+        </thead>
+   <tbody id="tbody">
+    <?php  
+        if($_GET["pg"] == null || !is_numeric($_GET["pg"]))
+        {
+            loadEmployers(1); 
+        }   
+        else
+        {
+        loadEmployers($_GET["pg"]);
+        }
+    ?>
+    </tbody>
+    </table>
+      <?php loadLink("SELECT * from employee"); ?>
+    </div>
+    <script>
+var modal = document.getElementById('full-options-block');
+var img = document.getElementById('imgclick');
+
+document.onclick = function(e){
+    if(e.target == img){
+        if(modal.style.display == 'block'){
+        modal.style.display = 'none';
+    }
+    else{
+        modal.style.display = 'block';
+    }
+    }
+    else{
+          modal.style.display = 'none';
+    }
+}
+</script>
+</body>
+</html>
